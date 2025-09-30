3df8966f666298e53760e261cb507dd4bba3c39a
Khang Doan <kdoan.personal@gmail.com>
2025-09-30 01:37:53 -0500

    added WelcomeMessage.java

--- runners/src/main/java/dev/khang/runners/Application.java
+++ runners/src/main/java/dev/khang/runners/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RunnersApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnersApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ runners/src/main/java/foo/bar/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package foo.bar;
+
+public class WelcomeMessage {
+    
+    public String getWelcomeMessage() {
+        return "Welcome to the Spring Boot Application!";
+    }
+
+}
